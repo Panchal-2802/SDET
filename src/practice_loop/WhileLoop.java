adea4735d4f982a351b41323ca2b414f13e66142
LENOVO <LENOVO@DESKTOP-6SR3BOL>
2025-05-18 10:57:31 +0530

    Single_dimension array

---
+++ src/practice_loop/WhileLoop.java
@@ -0,0 +1,20 @@
+package practice_loop;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		 int i = 1;
+		 while(true) {
+			 System.out.println("hello");
+			 i++;
+			 
+			 if(i==5) {
+				 break;
+			 }
+
+		 }
+		 
+		
+	}
+
+}
---
+++ src/singleDimensionArray/Single_dimension.java
@@ -0,0 +1,23 @@
+package singleDimensionArray;
+/*Declarung array & assigment value to array
+ * 
+ * 
+ * */
+public class Single_dimension {
+
+	public static void main(String[] args) {
+		//declaring an array
+		//approach - 1
+		int a[]=new int[5];
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		a[3]=400;
+		a[4]=500;
+		
+		// approach - 2
+		int b[]= {100,200,300,400,500};
+
+	}
+
+}
