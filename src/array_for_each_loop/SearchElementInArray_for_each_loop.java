488b4738b67653b9690ee8c9d5d3f1cf046d3c07
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 11:46:31 +0530

    search element in array by linear method via for each loop

---
+++ src/array_for_each_loop/SearchElementInArray_for_each_loop.java
@@ -0,0 +1,22 @@
+package array_for_each_loop;
+
+public class SearchElementInArray_for_each_loop {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,5,6,4};
+		int search_element=8;
+		boolean status = false;
+		for(int x:a) {
+			if(x==search_element) {
+				System.out.println("Element found in an array : " + x);
+				status=true;
+				break;
+			}
+		}
+		if(status==false) {
+			System.out.println("Element not found in array : " + search_element);
+		}
+
+	}
+
+}
