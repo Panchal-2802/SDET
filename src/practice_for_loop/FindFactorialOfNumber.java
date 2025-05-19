f0c85f3790b539a4f60c96420e362dab75d985c7
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:12:16 +0530

    factorial of N number via for loop

--- src/practice_for_loop/FindFactorialOfNumber.java
+++ src/practice_for_loop/FindFactorialOfNumber.java
@@ -7,9 +7,12 @@ public class FindFactorialOfNumber {
 		int factorial = 1;
 		for(int i=1; i<=number; i++) {
 			 factorial*=i;// factorial = factorial * i
-			              // fact = 1
-			              // 2*1=2
-			              //fact = 2
+			              // fact = 1*1=1
+			              // fact = 1*2=2
+			              //fact = 3*2=6
+			              //fact = 6*4=24
+			              //fact = 5*24=120
+			              //fact = 6*120=720
 		}
 		System.out.println("The Factorial of " + number + " is : " + factorial);
 
