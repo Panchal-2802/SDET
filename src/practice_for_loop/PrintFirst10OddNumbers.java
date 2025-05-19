d18b00894abff4698d61215acbc1d4689879693a
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:11:25 +0530

    first 10 odd number via for loop

--- src/practice_for_loop/PrintFirst10OddNumbers.java
+++ src/practice_for_loop/PrintFirst10OddNumbers.java
@@ -3,11 +3,14 @@ package practice_for_loop;
 public class PrintFirst10OddNumbers {
 
 	public static void main(String[] args) {
+		int count=0;
 		for(int i = 1; i<=20; i++) {
 			if(i%2!=0) {
 				System.out.println("Odd Number : " + i);
 			}
+			count++;
 		}
+		System.out.println();
 		  
 	}
 
