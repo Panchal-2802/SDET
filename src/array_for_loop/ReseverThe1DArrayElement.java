5269f4c23a5488f27f8666599fbc16560f719da0
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 15:34:17 +0530

    Reverse nnumber via for loop

---
+++ src/array_for_loop/ReseverThe1DArrayElement.java
@@ -0,0 +1,13 @@
+package array_for_loop;
+
+public class ReseverThe1DArrayElement {
+
+	public static void main(String[] args) {
+		int a[]= {6,5,4,3,2,1};
+		for(int i=5; i>=0; i--) {
+			System.out.print(a[i]);
+		}
+		
+	}
+
+}
