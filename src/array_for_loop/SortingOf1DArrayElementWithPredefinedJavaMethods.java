7f15f7dea44ab291da5c8cf42350f3d9022961a5
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 15:17:09 +0530

    sorting of integer 1d array with predefined java methods

---
+++ src/array_for_loop/SortingOf1DArrayElementWithPredefinedJavaMethods.java
@@ -0,0 +1,15 @@
+package array_for_loop;
+
+import java.util.Arrays;
+
+public class SortingOf1DArrayElementWithPredefinedJavaMethods {
+
+	public static void main(String[] args) {
+		int a[]= {6,5,4,3,2,1};
+		System.out.println("before sorting :- " + Arrays.toString(a));
+		Arrays.sort(a);
+		System.out.println("after sorting :- " + Arrays.toString(a));
+
+	}
+
+}
