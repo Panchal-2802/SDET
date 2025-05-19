361876856bf2f48d8fd3813955f1412abfa0cd04
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:13:46 +0530

    check number is prime or not via for loop

--- src/practice_for_loop/CheckNumberIsPrimeOrNot.java
+++ src/practice_for_loop/CheckNumberIsPrimeOrNot.java
@@ -3,8 +3,8 @@ package practice_for_loop;
 public class CheckNumberIsPrimeOrNot {
 
 	public static void main(String[] args) {
-		int prime = 1;
-		for(int i=prime; i!=0; i++) {
+		int prime = 2;
+		for(int i=1; i<=prime; i++) {
 			if(i%prime ==0 && i%1==0) {
 				System.out.println("The Given number is prime : " + i);
 			}
