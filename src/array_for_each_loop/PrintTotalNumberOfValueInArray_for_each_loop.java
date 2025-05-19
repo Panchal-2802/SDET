0746f8c7a78fc6f239bbad017a18a5b620d0092a
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:10:18 +0530

    print total value of array via for each loop

---
+++ src/array_for_each_loop/PrintTotalNumberOfValueInArray_for_each_loop.java
@@ -0,0 +1,13 @@
+package array_for_each_loop;
+
+public class PrintTotalNumberOfValueInArray_for_each_loop {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4,5,6,7,8,8,8,9,3};
+		for(int x:a) {
+			System.out.print(x + " ");
+		}
+
+	}
+
+}
