4e7fa0362dcb0b9f1caa2ddc6d820b0ba843b3ed
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 14:43:46 +0530

    correction

--- src/array_for_loop/FindNumberOfDuplicationInArray.java
+++ src/array_for_loop/FindNumberOfDuplicationInArray.java
@@ -4,7 +4,7 @@ public class FindNumberOfDuplicationInArray {
 
 	public static void main(String[] args) {
 		int a[]= {1,1,2,36,1,1};
-		int search_result =36;
+		int search_result =1;
 		boolean status = false;
 		int count=0;
 		for(int i=0; i<=a.length-1; i++) {
@@ -12,7 +12,12 @@ public class FindNumberOfDuplicationInArray {
 				count++;
 			}
 		}
-		System.out.println("Total number of duplicate " + search_result + " are find in array : " + count);
+		if (count > 0) {
+            System.out.println(search_result + " appears " + count + " time(s) in the array.");
+        } else {
+            System.out.println(search_result + " is not found in the array.");
+        }
+		//System.out.println("Total number of duplicate of " + search_result + " are find in array : " + count);
 	}
 
 }
