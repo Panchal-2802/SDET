04dc12e3434e39f4ab24f32925feb83cc5ca9c57
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:13:02 +0530

    multiplication of table 1 to 10 via for loop

--- src/practice_for_loop/MultiplicationOfTable1To10.java
+++ src/practice_for_loop/MultiplicationOfTable1To10.java
@@ -3,12 +3,12 @@ package practice_for_loop;
 public class MultiplicationOfTable1To10 {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=15; i++) {
+		for(int i=1; i<=10; i++) {
 			System.out.println("Multiplication of table " + i + ":");
 			for(int j=1; j<=10;j++) {
 				System.out.println(i + " x " + j + " = " + (i * j));
 			}
-		}
+		}//j++
 		System.out.println();
 
 	}
