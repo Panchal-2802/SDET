adcc36221b378ccc68ed30856f0251c7421592cd
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 12:12:33 +0530

    reverse number via for loop

--- src/practice_for_loop/ReverseNumber.java
+++ src/practice_for_loop/ReverseNumber.java
@@ -6,9 +6,15 @@ public class ReverseNumber {
 		int digit = 456789;
 		int reversed = 0;
 		for(int i = digit; i!=0; i=i/10) {
-			int num = i%10;
-			reversed = reversed * 10 + num;
-		}
+			int num = i%10;// num = 9
+			               // 45678%10=8
+			               //4567%10=7
+			reversed = reversed * 10 + num; // 0+9 = 9
+			                                //9*10+8=98
+			                                //98*10+7=980+7=987
+		}//i=i/10 = 456789/10 =45678
+		         // =45678/10=4567
+		         //4567/10=
 		System.out.println("Reversed Number : " + reversed);
 
 	}
