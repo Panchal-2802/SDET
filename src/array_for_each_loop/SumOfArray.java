682a0266ad88677be6bbdd947e2ad3f2021de122
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-19 14:18:50 +0530

    dry run

--- src/array_for_each_loop/SumOfArray.java
+++ src/array_for_each_loop/SumOfArray.java
@@ -6,7 +6,11 @@ public class SumOfArray {
 		int a[] = {1,2,3,4,5};
 		int sum=0;
 		for(int x:a) {
-			sum+=x;
+			sum+=x;// 0+1=1
+			       //1+2=3
+			       //3+3=6
+			       //6+4=10
+			       //10+5=15
 		}
 		System.out.println("The sum of array is : " + sum);
 
