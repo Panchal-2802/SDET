3406094ad23e1bbe62761abadd87135bb94c3720
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-21 14:31:24 +0530

    approach 2

--- src/array_for_loop/FindSecondLargestNumberInArray.java
+++ src/array_for_loop/FindSecondLargestNumberInArray.java
@@ -18,6 +18,20 @@ public class FindSecondLargestNumberInArray {
 			}
 		}
 		System.out.println("The second max element of array is : " + second_max);
+		
+		int a[]= {4,9,2,6,1,7,10};
+		int max1=a[0];
+		int sec_max=a[0];
+		for(int i=0; i<a.length; i++) {
+			if(a[i]>max1) {
+				sec_max=max1;
+				max1=a[i];
+			}
+			else if(a[i]>sec_max && a[i]!=max1) {
+				sec_max=a[i];
+			}
+		}
+		System.out.println("The second max element by approach - 2 : " + sec_max);
 	}
 
 }
