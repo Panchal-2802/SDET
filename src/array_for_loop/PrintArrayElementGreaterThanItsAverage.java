9b28c68e12976bf9b3bfcde4f9c5d9c7f5eeaca6
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-22 09:30:24 +0530

    printing arrray element greater than its average value

--- src/array_for_loop/PrintArrayElementGreaterThanItsAverage.java
+++ src/array_for_loop/PrintArrayElementGreaterThanItsAverage.java
@@ -3,7 +3,19 @@ package array_for_loop;
 public class PrintArrayElementGreaterThanItsAverage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int nums[] = {5, 3, 8, 6, 2, 7, 4, 1};
+        double sum2=0;
+		for(int i=0; i<nums.length; i++) {
+			sum2+=nums[i];
+		}
+		double avg=sum2/nums.length;
+		System.out.println("Average of array element : " + avg);
+		System.out.print("Number greater than average value are : ");
+		for(int i=0; i<nums.length; i++) {
+			if(nums[i]>avg) {
+				System.out.print(nums[i]+" ");
+			}
+		}
 
 	}
 
