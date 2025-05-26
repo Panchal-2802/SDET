e047c7c5a2a20ab79d40c6f2802f284fdbf17077
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-26 14:09:41 +0530

    changes

--- src/array_for_loop/SortingOf1DArrayElementWithPredefinedJavaMethods.java
+++ src/array_for_loop/SortingOf1DArrayElementWithPredefinedJavaMethods.java
@@ -9,7 +9,28 @@ public class SortingOf1DArrayElementWithPredefinedJavaMethods {
 		System.out.println("before sorting :- " + Arrays.toString(a));
 		Arrays.sort(a);
 		System.out.println("after sorting :- " + Arrays.toString(a));
+		
+		/*int b[]= {5,2,8,7,1};
+		System.out.print("Original Array : " );
+		for(int i=0; i<b.length; i++) {
+			System.out.print(b[i] + " ");
+			
+			int temp=0;
+			for(int j=0; j<b.length; j++) {
+				for(int k=j+1; k<b.length; k++) {
+					if(b[j]<b[i]) {
+						temp = b[i];
+						b[i]=b[j];
+						b[j]=temp;
+					}
+				}
+			}
+		}
+		System.out.print("sorted array : " );
+		for(int i=0; i<b.length; i++) {
+			System.out.print(b[i] + " ");
 
-	}
+	}*/
 
+}
 }
