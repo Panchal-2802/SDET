69b0653f4181919c6e0fe819757b523e78f4b421
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-26 11:32:55 +0530

    changes

--- src/Switch_case/Calculator.java
+++ src/Switch_case/Calculator.java
@@ -1,30 +1,40 @@
 package Switch_case;
 
 public class Calculator {
+//Day-8
 //Create a simple calculator program using 
 //switch case that performs: +, -, *, /, % based on operator input.
 	public static void main(String[] args) {
 		int a=10;
-		int b=20;
+		int b=0;
 		String operator = "/";
-		switch(operator) {
-		case "+" : 
-			System.out.println("The addition of a & b : " + (a+b));break;
-		case "-" :
-			System.out.println("The substraction of a & b : " + (a-b));break;
-		case "*" :
-			System.out.println("The multiplication of a & b : " + (a*b));break;
-		case "/" :
-			System.out.println("The divide of a & b : " + (a/b));break;
-		case "%" :
-			System.out.println("The multiplication of a & b : " + (a%b));break;
-			default:
-				System.out.println("Not entered a valid input");
+          switch(operator) {
+	            case "+" : 
+	                System.out.println("The addition of a & b : " + (a + b));
+	                break;
+	            case "-" :
+	                System.out.println("The subtraction of a & b : " + (a - b));
+	                break;
+	            case "*" :
+	                System.out.println("The multiplication of a & b : " + (a * b));
+	                break;
+	            case "/" :
+	                if (b != 0) {
+	                    System.out.println("The division of a & b : " + (a / b));
+	                } else {
+	                    System.out.println("Error: Division by zero is not allowed.");
+	                }
+	                break;
+	            case "%" :
+	                if (b != 0) {
+	                    System.out.println("The remainder of a & b : " + (a % b));
+	                } else {
+	                    System.out.println("Error: Modulo by zero is not allowed.");
+	                }
+	                break;
+	            default:
+	                System.out.println("Not a valid operator");
 		}
 		
-			
-			
-
 	}
-
 }
