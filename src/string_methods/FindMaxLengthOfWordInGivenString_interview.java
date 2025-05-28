abc722e14b65028504c8bb2c5206d06d2d9687b7
Admin <Admin@DESKTOP-HQEF4C9>
2025-05-28 09:32:01 +0530

    Interview

--- src/string_methods/FindMaxLengthOfWordInGivenString_interview.java
+++ src/string_methods/FindMaxLengthOfWordInGivenString_interview.java
@@ -12,7 +12,7 @@ public class FindMaxLengthOfWordInGivenString_interview {
 		String s2[]=s1.split(" ");
 		String s3=s2[0];
 		//System.out.println(Arrays.toString(s2));
-		for (int i=1; i<s2.length; i++) {
+		for (int i=0; i<s2.length; i++) {
 			if(s2[i].length()>s3.length()) {
 				s3=s2[i];
 			}
