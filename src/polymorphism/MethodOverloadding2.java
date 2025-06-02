75caf16b7ae19cfceb2ccb3c7eb200f44d25dffb
LENOVO <LENOVO@DESKTOP-6SR3BOL>
2025-06-02 14:03:56 +0530

    calling variable of polymorphism -> methodoverloadding

---
+++ src/polymorphism/MethodOverloadding2.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class MethodOverloadding2 {
+
+	public static void main(String[] args) {
+		MethodOverloadding obj1 = new MethodOverloadding();
+		obj1.method(20, 40);
+		obj1.method(25, 2520d);
+		
+		
+
+	}
+
+}
